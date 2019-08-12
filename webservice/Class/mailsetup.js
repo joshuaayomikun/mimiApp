5b1c65e9736b966284623be5cca86dea3d4319b6
Akande Joshua <jaakande@sidmach.com>
2019-08-12 16:58:09 +0100

    Update mailsetup.js

--- webservice/Class/mailsetup.js
+++ webservice/Class/mailsetup.js
@@ -1,6 +1,6 @@
 
 const MailClass = class{
-    constructor(service = "gmail", user="joshuaayomikun@gmail.com", pass="toyinakande2"){
+    constructor(service = "gmail", user="joshuaayomikun@gmail.com", pass="************"){
         this.service = service
         this.auth = {
             user:user, pass:pass
